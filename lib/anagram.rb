bffa41df3d7a6508ba229a302f3c49a0bd8eb2a1
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-06 18:25:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word)
+   word.collect do |w|
+     w.compare
+  end
+  
+end
